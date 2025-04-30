51d334406e7a74992cc32830d133c332585fb5b0
Theron <theron.halsey@gmail.com>
2025-04-30 13:42:11 -0400

    added styling for different sized media

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ import PageHeader from './components/PageHeader.vue'
 <style scoped>
 #header {
   position: fixed;
-  top: 0;
+  top: 2rem;
   left: 0;
   line-height: 1.5;
   max-height: 100vh;
@@ -47,6 +47,7 @@ nav {
   font-size: 2rem;
   text-align: center;
   margin-top: 2rem;
+  padding: 1rem 0;
 }
 
 nav a.router-link-exact-active {
@@ -75,13 +76,72 @@ nav a:first-of-type {
   text-align: center;
 }
 
-@media (min-width: 1024px) {
+@media (max-width: 1280px) {
+  #header {
+    top: 1rem;
+  }
+
   nav {
     text-align: left;
     margin-left: -1rem;
-    font-size: 2rem;
+    font-size: 1.5rem;
     padding: 1rem 0;
     margin-top: 1rem;
+  }
+
+  #main {
+    position: fixed;
+    margin: auto;
+    width: 100%;
+    height: 70%;
+    place-items: center;
+    transform: translate(-50%, -45%);
+  }
+}
+
+@media (max-width: 810px) {
+  #header {
+    top: 1rem;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1.5rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+
+  #main {
+    position: fixed;
+    margin: auto;
+    width: 97%;
+    height: 72%;
+    place-items: center;
+    transform: translate(-2.5%, 15%);
+  }
+}
+
+@media (max-width: 500px) {
+  #header {
+    top: 0.5rem;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: 0rem;
+    font-size: 0.7rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+
+  #main {
+    position: fixed;
+    margin: auto;
+    width: 97%;
+    height: 72%;
+    place-items: center;
+    transform: translate(-7%, 7.5%);
   }
 }
 </style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -11,7 +11,25 @@ defineProps<{
 <style scoped>
 h1 {
   font-weight: 500;
-  font-size: 2.3rem;
+  font-size: 2.8rem;
   text-align: center;
+}
+
+@media (max-width: 1280px) {
+  h1 {
+    font-size: 2.1rem;
+  }
+}
+
+@media (max-width: 810px) {
+  h1 {
+    font-size: 2.1rem;
+  }
+}
+
+@media (max-width: 500px) {
+  h1 {
+    font-size: 1rem;
+  }
 }
 </style>
